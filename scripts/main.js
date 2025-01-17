ed296ef542307bd3d2507696c47c1e4bf0081606
sampano <samuelpano@gmail.com>
2025-01-17 13:21:29 +0800

    Added More Styling

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  // Cache the header for performance
+  var header = $(".header");
+
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() > 50) {
+      header.css("background-color", "#000"); // Change to black
+    } else {
+      header.css("background-color", "rgba(0, 0, 0, 0)"); // Reset to transparent
+    }
+  });
+});
+$(document).ready(function () {
+  $(".burger-menu").on("click", function () {
+    const isOpen = $(".header__content--links-mobile-container").is(":visible");
+    if (isOpen) {
+      // Close menu
+      $(".burger-icon").show();
+      $(".close-icon").hide();
+    } else {
+      // Open menu
+      $(".burger-icon").hide();
+      $(".close-icon").show();
+    }
+    $(".header__content--links-mobile-container").slideToggle();
+  });
+});
