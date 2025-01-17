ef63ebdeb5dc1a646df703f715e02be7deab4ea7
sampano <samuelpano@gmail.com>
2025-01-17 15:36:17 +0800

    Style Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,8 @@
 $(document).ready(function () {
-  // Cache the header for performance
-  //   var header = $(".header");
-
-  //   $(window).on("scroll", function () {
-  //     if ($(this).scrollTop() > 50) {
-  //       header.css("background-color", "#000"); // Change to black
-  //     } else {
-  //       header.css("background-color", "rgba(0, 0, 0, 0)"); // Reset to transparent
-  //     }
-  //   });
   var header = $(".header");
   handleHeaderScroll(header);
   toggleMobileMenu();
 });
-// $(document).ready(function () {
-//   $(".burger-menu").on("click", function () {
-//     const isOpen = $(".header__content--links-mobile-container").is(":visible");
-//     if (isOpen) {
-//       // Close menu
-//       $(".burger-icon").show();
-//       $(".close-icon").hide();
-//     } else {
-//       // Open menu
-//       $(".burger-icon").hide();
-//       $(".close-icon").show();
-//     }
-//     $(".header__content--links-mobile-container").slideToggle();
-//   });
-// });
 
 function handleHeaderScroll(header) {
   $(window).on("scroll", function () {
