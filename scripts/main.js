c8e894984c36c8759a0bba2312c3a42232c41530
sampano <samuelpano@gmail.com>
2025-01-17 15:33:06 +0800

    Styling Update

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,45 @@
 $(document).ready(function () {
   // Cache the header for performance
-  var header = $(".header");
+  //   var header = $(".header");
 
+  //   $(window).on("scroll", function () {
+  //     if ($(this).scrollTop() > 50) {
+  //       header.css("background-color", "#000"); // Change to black
+  //     } else {
+  //       header.css("background-color", "rgba(0, 0, 0, 0)"); // Reset to transparent
+  //     }
+  //   });
+  var header = $(".header");
+  handleHeaderScroll(header);
+  toggleMobileMenu();
+});
+// $(document).ready(function () {
+//   $(".burger-menu").on("click", function () {
+//     const isOpen = $(".header__content--links-mobile-container").is(":visible");
+//     if (isOpen) {
+//       // Close menu
+//       $(".burger-icon").show();
+//       $(".close-icon").hide();
+//     } else {
+//       // Open menu
+//       $(".burger-icon").hide();
+//       $(".close-icon").show();
+//     }
+//     $(".header__content--links-mobile-container").slideToggle();
+//   });
+// });
+
+function handleHeaderScroll(header) {
   $(window).on("scroll", function () {
     if ($(this).scrollTop() > 50) {
-      header.css("background-color", "#000"); // Change to black
+      header.css("background-color", "#000");
     } else {
-      header.css("background-color", "rgba(0, 0, 0, 0)"); // Reset to transparent
+      header.css("background-color", "rgba(0, 0, 0, 0)");
     }
   });
-});
-$(document).ready(function () {
+}
+
+function toggleMobileMenu() {
   $(".burger-menu").on("click", function () {
     const isOpen = $(".header__content--links-mobile-container").is(":visible");
     if (isOpen) {
@@ -24,4 +53,4 @@ $(document).ready(function () {
     }
     $(".header__content--links-mobile-container").slideToggle();
   });
-});
+}
